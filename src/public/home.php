3a245cfd06113c152a1de4bed0a0b53ec4118570
codevbk <123780171+codevbk@users.noreply.github.com>
2023-04-25 01:47:19 +0300

    Added OOP-MVC Refactor
    - Cleanup code
    - Added Model-View-Controller class & functions

--- src/public/home.php
+++ src/public/home.php
@@ -41,69 +41,100 @@ while(isset(ASSETS_ONLINE_JS[$i])){
 var count = 0;
 function datetime_now(){
     var date = new Date();
-var dateStr =
-  date.getFullYear() + "-" +("00" + (date.getMonth() + 1)).slice(-2) + "-" +("00" + date.getDate()).slice(-2) + " " +
-  ("00" + date.getHours()).slice(-2) + ":" +
-  ("00" + date.getMinutes()).slice(-2) + ":" +
-  ("00" + date.getSeconds()).slice(-2);
-  return dateStr;
+    var dateStr =
+    date.getFullYear() + "-" +("00" + (date.getMonth() + 1)).slice(-2) + "-" +("00" + date.getDate()).slice(-2) + " " +
+    ("00" + date.getHours()).slice(-2) + ":" +
+    ("00" + date.getMinutes()).slice(-2) + ":" +
+    ("00" + date.getSeconds()).slice(-2);
+    return dateStr;
 }
-//console.log(datetime_now());
-$(document).on("click","#note_save",function() {
-    var noteList = $('#note_list');
-    var noteEditElement = $(this).parent().parent();
-    var noteEditContent = $(noteEditElement).find('#note_edit_content');
-    var noteID = $(noteEditElement).attr("data-id");
-    if(noteID === ''){
-        var noteClone = $(noteList).find(">:first-child").clone();
-        $(noteClone).attr("style","display:block;");
-        $(noteClone).attr("data-id", count);
-        $(noteClone).find("#note_title").text(datetime_now());
-        $(noteClone).find("#note_content").text($(noteEditContent).val());
-        $(noteEditElement).attr("data-id","");
-        $(noteEditElement).find("#note_edit_title").text("");
-        $(noteEditContent).val("");
-        $(noteList).append(noteClone);
-        count++
-    }else{
-        var noteElement = $(noteList).find( "[data-id='"+noteID+"']" );
-        $(noteElement).find("#note_title").text(datetime_now());
-        $(noteElement).find("#note_content").text($(noteEditContent).val());
-        $(noteEditElement).attr("data-id","");
-        $(noteEditElement).find("#note_edit_title").text("");
-        $(noteEditContent).val("");
-        //console.log($(noteElement).html());
-    }
-    //console.log($(noteList).find(">:first-child").html());
+// Model
+class NoteModel {
+	constructor() {
+        this.NoteCount = 0;
+		this.NoteList = [[]];
+	}
 
+	addNoteItem(noteID,noteTitle,NoteContent) {
+        this.NoteList.push([]);
+		this.NoteList[this.NoteCount].push(this.NoteCount);
+		this.NoteList[this.NoteCount].push(noteTitle);
+		this.NoteList[this.NoteCount].push(NoteContent);
+        this.NoteCount++;
+	}
 
-});
-$(document).on("click","#note_edit",function() {
-    var noteList = $('#note_list');
-    var noteEditContent = $('#note_edit_content');
-    var noteEditElement = $(noteEditContent).parent().parent();
-    var noteElement = $(this).parent().parent().parent();
-    var noteElementID = $(noteElement).attr("data-id");
-    var noteElementTitle = $(noteElement).find("#note_title").text();
-    var noteElementContent = $(noteElement).find("#note_content").text();
-    $(noteEditElement).attr("data-id", noteElementID);
-    $(noteEditElement).find("#note_edit_title").text(noteElementTitle);
-    $(noteEditElement).find("#note_edit_content").val(noteElementContent);
-    //console.log(noteID);
-});
-$(document).on("click","#note_delete",function() {
-    var noteList = $('#note_list');
-    var noteElement = $(this).parent().parent().parent();
-    var noteID = $(noteElement).attr("data-id");
-    noteElement.remove();
-    var noteEditContent = $('#note_edit_content');
-    var noteEditElement = $(noteEditContent).parent().parent();
-    $(noteEditElement).attr("data-id","");
-    $(noteEditElement).find("#note_edit_title").text("");
-    $(noteEditContent).val("");
-    //console.log(noteID);
-});
+	getNoteList() {
+		return this.NoteList;
+	}
+}
 
+//View
+class NoteView {
+	constructor(noteModel) {
+		this.noteModel = noteModel;
+		this.noteListElement = $("#note_list");
+		this.noteContentElement = $("#note_edit_content");
+		this.noteSaveElement = $("#note_save");
+	}
+
+	renderNoteList() {
+		this.noteListElement.empty();
+		var noteList = this.noteModel.getNoteList();
+        for (var i = 0; i < noteList.length; i++) {
+            console.log(noteList);
+            this.noteListElement.append(
+                '<div class="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-4" data-id="'+noteList[i][0]+'">\
+                    <div class="card">\
+                    <div class="card-header">\
+                        Note :\
+                    </div>\
+                    <div class="card-body">\
+                        <h5 class="card-title">Note : <span id="note_title"> ' +noteList[i][1]+'</span></h5>\
+                        <p class="card-text" id="note_content">'+noteList[i][2]+'</p>\
+                    </div>\
+                    </div>\
+                </div>'
+            );
+        }
+	}
+
+	getNoteInputValue() {
+		return this.noteContentElement.val();
+	}
+
+	clearNoteInput() {
+		this.noteContentElement.val("");
+	}
+
+	addNoteItemHandler(callback) {
+		this.noteSaveElement.click(callback);
+	}
+}
+
+// Controller
+class NoteController {
+	constructor(noteModel, noteView) {
+		this.noteModel = noteModel;
+		this.noteView = noteView;
+		this.noteView.addNoteItemHandler(this.addNoteItem.bind(this));
+		this.renderNoteList();
+	}
+
+	renderNoteList() {
+		this.noteView.renderNoteList();
+	}
+
+	addNoteItem() {
+		var noteContent = this.noteView.getNoteInputValue();
+        this.noteModel.addNoteItem(null,datetime_now(),noteContent);
+        this.renderNoteList();
+        this.noteView.clearNoteInput();
+	}
+}
+
+var noteModel = new NoteModel();
+var noteView = new NoteView(noteModel);
+var noteController = new NoteController(noteModel, noteView);
 </script>
 </body>
 </html>
