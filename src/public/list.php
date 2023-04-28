793cf7bf22657645f6effd1e59f8babf3b2bdc91
codevbk <123780171+codevbk@users.noreply.github.com>
2023-04-28 19:52:03 +0300

    Cleanup code

--- src/public/list.php
+++ src/public/list.php
@@ -12,19 +12,6 @@
       </div>
       <div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
         <div class="row" id="note_list">
-          <div class="col-12 col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 mb-4" data-id="" style="display:none;">
-            <div class="card">
-              <div class="card-header">
-                Note :
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Note : <span id="note_title"></span></h5>
-                <p class="card-text" id="note_content"></p>
-                <a href="javascript:;" class="btn btn-primary" id="note_edit"><i class="bi bi-pencil-square"></i></a>
-                <a href="javascript:;" class="btn btn-primary" id="note_delete"><i class="bi bi-trash"></i></a>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
